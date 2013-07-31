ac519291d75a31f527701aff870cdf6e3449d951
Markus Kilås <markus@markuspage.com>
2013-07-31 11:14:23 +0200

    Added source code headers.

--- android-timetracker/src/com/markuspage/android/atimetracker/Preferences.java
+++ android-timetracker/src/com/markuspage/android/atimetracker/Preferences.java
@@ -1,8 +1,23 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * A Time Tracker - Open Source Time Tracker for Android
+ *
+ * Copyright (C) 2013  Markus Kilås <markus@markuspage.com>
+ * Copyright (C) 2008, 2009, 2010  Sean Russell <ser@germane-software.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 package com.markuspage.android.atimetracker;
 
 import java.util.ArrayList;
