cc400c7890a786e982c58d335d749490957249fd
Markus Kilås <markus@kilas.se>
2018-12-28 16:52:06 +0100

    Implement proper equals and hashCode.

--- android-timetracker/app/src/main/java/com/markuspage/android/atimetracker/Activity.java
+++ android-timetracker/app/src/main/java/com/markuspage/android/atimetracker/Activity.java
@@ -105,8 +105,29 @@ public class Activity implements Comparable<Activity> {
         this.endTime = endTime;
     }
 
-    public boolean equals(Activity other) {
-        return other != null && other.getId() == id;
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Activity other = (Activity) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
     }
 
     @Override
